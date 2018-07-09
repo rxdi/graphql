3b64dedc62d46b92e2a5e83968e0510eb94f20bf
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-07-10 00:02:16 +0300

    chore(minify): bundled module only typings left

--- config.tokens.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@rxdi/core");
-exports.GRAPHQL_PLUGIN_CONFIG = new core_1.InjectionToken('graphql-configuration-injection-token');

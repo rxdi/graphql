6c43056b20bfee1e280be3aae5ca553ab3c48cf5
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-09-07 23:04:39 +0300

    test(Guard,Interceptor): added more tests to guards and interceptors

--- development/decorators/guard/guard.decorator.spec.ts
+++ development/decorators/guard/guard.decorator.spec.ts
@@ -8,6 +8,8 @@ export class AuthGuard implements CanActivateResolver {
 
     canActivate(
         context: boolean,
+        payload,
+        descriptor
     ): boolean | Promise<boolean> | Observable<boolean> {
         return context;
     }
@@ -29,6 +31,42 @@ describe('Decorators: @Guard', () => {
         expect(query.findUser().guards[0]['metadata']['moduleHash']).toBe(AuthGuard['metadata']['moduleHash']);
         expect(currentGuard.canActivate).toBeDefined();
         expect(currentGuard.canActivate(false)).toBeFalsy();
+        done();
+    });
+});
+
+
+
+describe('Decorators: @Guard', () => {
+    it('Should decorate findUser to have guard and to check if conditions', (done) => {
+        @Service()
+        class AuthGuard implements CanActivateResolver {
+
+            canActivate(
+                context: boolean,
+                payload,
+                descriptor
+            ): boolean | Promise<boolean> | Observable<boolean> {
+                if (payload === 'test') {
+                    return true;
+                }
+            }
+        }
+        @Controller()
+        class TestController {
+            @Guard(AuthGuard)
+            @Query()
+            findUser() {
+                return 1;
+            }
+        }
+        const controller: { findUser(): { guards: Function[] } } = <any>Container.get(TestController);
+        const currentGuard = Container.get<CanActivateResolver>(controller.findUser().guards[0]);
+        const descriptor = controller.findUser();
+        expect(controller.findUser().guards[0]['metadata']['moduleHash']).toBe(AuthGuard['metadata']['moduleHash']);
+        expect(currentGuard.canActivate).toBeDefined();
+        expect(currentGuard.canActivate(null, 'test')).toBeTruthy();
+        expect(currentGuard.canActivate(null, 'no-test')).toBeFalsy();
         done();
     });
 });
